008c02f3066c34f311dba532dcb7ee4afa760fdd
borastudy <borastudy@gmail.com>
2020-05-20 23:54:50 +0700

    add scripts

---
+++ Assets/Scripts/BrainScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public enum MathOperator
+{
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DEVIDE
+}
+
+public class BrainScript : MonoBehaviour
+{
+    private float operandA;
+    private float operandB;
+    public float correctResult;
+    public float[] results;
+    public string question;
+    public MathOperator mathOperator;
+
+    public void GenerateQuiz ()
+    {
+        operandA = Random.Range (10, 50);
+        operandB = Random.Range (10, 50);
+
+        mathOperator = (MathOperator)Random.Range (0, 4);
+        
+        results = new float[4];
+        
+        string sign = "";
+        switch(mathOperator)
+        {
+            
+            case MathOperator.PLUS:
+            correctResult = operandA + operandB;
+            sign = " + ";
+            break;
+
+            case MathOperator.MINUS:
+            correctResult = operandA - operandB;
+            sign = " - ";
+            break;
+            
+            case MathOperator.MULTIPLY:
+            correctResult = operandA * operandB;
+            sign = " * ";
+            break;
+            
+            case MathOperator.DEVIDE:
+            correctResult = operandA / operandB;
+            sign = " / ";
+            break;
+        }
+
+        question = operandA + sign + operandB + " = ?";
+
+        results[0] = correctResult;
+        GenFakeResults();
+    }
+
+    private void GenFakeResults ()
+    {
+        for(int i = 1; i<4; i++)
+        {
+            float fakeResult = Random.Range (correctResult - 10, correctResult + 10);
+            while(fakeResult == correctResult || results.Contains(fakeResult))
+            {
+                fakeResult = Random.Range (correctResult - 10, correctResult + 10);
+            }
+
+            results[i] = fakeResult;
+        }
+    }
+
+}
---
+++ Assets/Scripts/GamePlayControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class GamePlayControl : MonoBehaviour
+{
+    [SerializeField] BrainScript brain;
+    [SerializeField] UnityEngine.UI.Text textQuestion;
+    [SerializeField] UnityEngine.UI.Text[] textAnswers;
+    
+
+    void Start()
+    {
+        RefreshQuiz ();
+    }
+
+    void RefreshQuiz ()
+    {
+        brain.GenerateQuiz();
+        textQuestion.text = brain.question;
+        textAnswers[0].text = brain.results[0].ToString();
+        textAnswers[1].text = brain.results[1].ToString();
+        textAnswers[2].text = brain.results[2].ToString();
+        textAnswers[3].text = brain.results[3].ToString();
+        textQuestion.text = brain.question;
+    }
+
+    public void AnswerClicked (int buttonIndex)
+    {
+        float answerFromPlayer = brain.results[buttonIndex];
+
+        if (answerFromPlayer == brain.correctResult)
+        {
+            Debug.Log("Correct !!");
+        }
+        else
+        {
+            Debug.Log("Wrong !!");
+        }
+
+        RefreshQuiz();
+    }
+
+}
