8714ddcdc7402947b1ae6600cd671c91dc0060d6
borastudy <borastudy@gmail.com>
2020-06-16 23:14:13 +0700

    Added brain scripts

---
+++ Assets/Scripts/BrainScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ enum MathOperator
+ {
+     PLUS,
+     MINUS,
+     MULTIPLY,
+     DEVIDE
+ }
+
+public class BrainScript : MonoBehaviour
+{
+    private int operandA;
+    private int operandB;
+    private int correctResult;
+    private int[] results;
+    private string question;
+    private MathOperator mathOperator;
+
+    public void GenerateQuiz ()
+    {
+        operandA = Random.Range (10, 50);
+        operandB = Random.Range (10, 50);
+
+        mathOperator = (MathOperator)Random.Range(0, 4);
+
+        question = operandA + mathOperator + operandB + " = ?";
+    }
+}
