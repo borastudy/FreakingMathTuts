30e7f83581d1465f8f9d1ba9fb271851e2354dac
borastudy <borastudy@gmail.com>
2020-06-16 23:53:41 +0700

    Update brain script to display question correctly in console

--- Assets/Scripts/BrainScript.cs
+++ Assets/Scripts/BrainScript.cs
@@ -19,6 +19,12 @@ public class BrainScript : MonoBehaviour
     private string question;
     private MathOperator mathOperator;
 
+    void Start ()
+    {
+        Debug.Log ("In function start!");
+        GenerateQuiz();
+    }
+
     public void GenerateQuiz ()
     {
         operandA = Random.Range (10, 50);
@@ -26,6 +32,29 @@ public class BrainScript : MonoBehaviour
 
         mathOperator = (MathOperator)Random.Range(0, 4);
 
-        question = operandA + mathOperator + operandB + " = ?";
+        string sign = "";
+        switch(mathOperator)
+        {
+            case MathOperator.PLUS:
+            sign = "+";
+            break;
+
+            case MathOperator.MINUS:
+            sign = "-";
+            break;
+
+            case MathOperator.MULTIPLY:
+            sign = "*";
+            break;
+
+            case MathOperator.DEVIDE:
+            sign = "/";
+            break;
+        }
+
+        question = operandA + " " + sign + " " + operandB + " = ?";
+
+        Debug.Log ("Question : " + question);
+
     }
 }
