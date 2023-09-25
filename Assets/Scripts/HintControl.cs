8eb9a07dd71b25d70ddd976e3e73e6412d49ef93
cheabora <borastudy@gmail.com>
2023-09-25 08:49:53 +0700

    add more hints

--- Assets/Scripts/HintControl.cs
+++ Assets/Scripts/HintControl.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class HintControl : MonoBehaviour
 {
     [SerializeField] GameControl gameControl;
+    [SerializeField] TimerBarControl timerControl;
 
     public void DoGiveAnswer()
     {
         //need help from gamecontrol
         gameControl.GiveAnswer();
+    }
+
+    public void DoPauseTimer()
+    {
+        //need help from timerbarcontrol
+        timerControl.Pause();
     }
 }
