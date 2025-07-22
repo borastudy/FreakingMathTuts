4a1ff7e7b3212f10ed50a2340544d89c9bfe0868
boramacmini <borastudy@gmail.com>
2025-07-22 22:50:54 +0700

    feat: implement hint bomb button

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -9,6 +9,7 @@ namespace FreakingMath
     public class GameControl : MonoBehaviour
     {
         [SerializeField] GameObject[] hands;
+        [SerializeField] GameObject[] buttonAnswers;
     
         [SerializeField] Text textScore;
         [SerializeField] Text textQuestion;
@@ -23,6 +24,8 @@ namespace FreakingMath
         [SerializeField] GameOverControl gameOverControl;
         [SerializeField] TimerBarControl timerBarControl;
         [SerializeField] Image imageBonusLife;
+
+        [SerializeField] GameObject explosionPrefab;
     
         private GameDataManager gameDataManager;
     
@@ -39,19 +42,25 @@ namespace FreakingMath
     
             imageBonusLife.fillAmount = 0;
         }
-    
-        private void GenerateQuestion ()
+
+        private void GenerateQuestion()
         {
             brainScript.GenerateQuiz();
             textQuestion.text = brainScript.GetQuestion();
-    
+
             var results = brainScript.GetResults();
             correctResult = brainScript.GetCorrectResult();
-    
+
             textAnswer1.text = results[0].ToString();
             textAnswer2.text = results[1].ToString();
             textAnswer3.text = results[2].ToString();
             textAnswer4.text = results[3].ToString();
+            
+            for (int i = 0; i < buttonAnswers.Length; i++)
+            {
+                buttonAnswers[i].SetActive(true);
+            }
+
         }
     
         public void GiveAnswer()
@@ -60,14 +69,43 @@ namespace FreakingMath
             hands[correctIndex].SetActive(true);
             HandleClick(correctIndex);
         }
+
+        public void Explosion()
+        {
+            explosionPrefab.SetActive(true);
+            StartCoroutine(HideExplosion());
+
+            var correctIndex = brainScript.GetCorrectResultIndex();
+
+            var countHidden = 0;
+            for (int i = 0; i < buttonAnswers.Length; i++)
+            {
+                if (i != correctIndex)
+                {
+                    buttonAnswers[i].SetActive(false);
+                    countHidden++;
+                }
+                if (countHidden == 2)
+                {
+                    break; // only hide 2 buttons
+                }
+                
+            }
+        }
+
+        IEnumerator HideExplosion()
+        {
+            yield return new WaitForSeconds(1.0f);
+            explosionPrefab.SetActive(false);
+        }
     
-        public void HandleClick (int index)
+        public void HandleClick(int index)
         {
             Debug.Log("You click me : " + index);
             var results = brainScript.GetResults();
             var chosenNumber = results[index];
             Debug.Log("Number : " + chosenNumber);
-    
+
             if (chosenNumber == correctResult)
             {
                 //increase score
@@ -75,19 +113,19 @@ namespace FreakingMath
                 textScore.text = score.ToString();
                 GenerateQuestion();
                 Debug.Log("Score : " + score);
-    
+
                 countCorrect++;
-                if(countCorrect == 10)
+                if (countCorrect == 10)
                 {
                     lifeControl.AddLife();
                     countCorrect = 0;
                 }
-    
+
             }
             else
             {
                 countCorrect = 0;
-    
+
                 //use life control
                 lifeControl.RemoveLife();
                 if (lifeControl.StillHasLife())
@@ -100,13 +138,13 @@ namespace FreakingMath
                     timerBarControl.Pause();
                 }
             }
-    
+
             if (lifeControl.StillHasLife())
             {
                 //call to reset timerbar
                 timerBarControl.Reset();
             }
-    
+
             imageBonusLife.fillAmount = countCorrect / 10.0f;
         }
     
--- Assets/Scripts/HintControl.cs
+++ Assets/Scripts/HintControl.cs
@@ -7,17 +7,23 @@ namespace FreakingMath
     {
         [SerializeField] GameControl gameControl;
         [SerializeField] TimerBarControl timerControl;
-    
+
         public void DoGiveAnswer()
         {
             //need help from gamecontrol
             gameControl.GiveAnswer();
         }
-    
+
         public void DoPauseTimer()
         {
             //need help from timerbarcontrol
             timerControl.Pause();
+        }
+        
+        public void DoExplosion()
+        {
+            //need help from gamecontrol
+            gameControl.Explosion();
         }
     }
 }
